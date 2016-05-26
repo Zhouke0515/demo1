6b10127d088b42d97ebc4d1da1d453b6d514d9a3
252356262@qq.com <Zhouke0515>
2016-05-26 12:53:54 +0800

    123

--- src/demo/test/lang/StringDemo.java
+++ src/demo/test/lang/StringDemo.java
@@ -1,8 +1,30 @@
 package demo.test.lang;
 
+import java.util.Date;
+
+
+
+
 public class StringDemo {
 
 	public static void main(String[] args) {
 		String str;
+		System.out.println(String.format("十进制：%d", 100));
+		System.out.println(String.format("十六进制：%x", 100));
+		System.out.println(String.format("八进制：%o", 100));
+		
+		System.out.println(String.format("浮点数：%f", 100.5321321));
+		System.out.println(String.format("浮点数：%g", 100.5321321));
+		
+		System.out.println(String.format("指数：%e", 100.5321321));
+		
+		
+		Date date = new Date();
+		System.out.println(date);
+		System.out.println(String.format("日期：%tF", date));
+		System.out.println(String.format("时间：%tT", date));
+		System.out.println(String.format("一年的第：%tj天", date));
+		
+		System.out.println(String.format("1970-1-1 00:00:00 到现在所经过的秒数：%ts", date));
 	}
 }
